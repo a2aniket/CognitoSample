648f87429722a81ed3d299e4b5360cf18bcc92ea
Aniket Thorat <aniket_thorat2@persistent.com>
2022-09-29 18:23:28 +0530

    initial commit

---
+++ src/main/java/com/sample/cognito/exception/CustomException.java
@@ -0,0 +1,19 @@
+package com.sample.cognito.exception;
+
+public class CustomException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public CustomException(String message) {
+        super(message);
+    }
+
+    public CustomException(String message, Throwable cause) {
+        super(message, cause);
+    }
+	
+
+}
---
+++ src/test/java/com/sample/cognito/CognitoSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.cognito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognitoSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
